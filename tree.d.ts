66c6110eccf145952ec84283357771cb82447d0e
Iain Ballard <iain.ballard@gmail.com>
2014-09-24 09:07:56 +0100

    Skeleton

---
+++ tree.d.ts
@@ -0,0 +1,3 @@
+declare module "tree" {
+    export function test();
+}
---
+++ tree.js
@@ -0,0 +1,6 @@
+(function (provider) {
+    provider.test = function() {
+        console.log("Skeleton loaded OK");
+    }
+
+}).call(global || exports || this);
