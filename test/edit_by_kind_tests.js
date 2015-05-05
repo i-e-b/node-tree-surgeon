d42b1c49947c36501a713dbd6ffbfdbb6da76ad9
Anthony Gascoigne <anthony.gascoigne@sigma-systems.com>
2015-05-05 10:57:39 +0100

    method: forEachByKind
    Calls the supplied predicate for each node of the specified Kind

--- test/edit_by_kind_tests.js
+++ test/edit_by_kind_tests.js
@@ -3,23 +3,65 @@ var _ = require('lodash');
 
 var tree = require("../tree-surgeon.js");
 
-describe("Editing children by kind and filter function", function() {
-    it("should modify selected children but no other nodes", function(){
-        var input = {
-            "no":{"A":"B"},
-            "yes":{"A":"C"},
-            "neither":{"A":"D"}
-        };
-        var expected = {
-            "no":{"A":"B"},
-            "yes":{"A":"C", "Found":"C"},
-            "neither":{"A":"D"}
-        };
+describe("Iterating over defined sets", function() {
 
-        var result = tree.compose(
-            tree.editByKind("yes", function(n){n["Found"] = n["A"]; return n;},
-                tree.decompose(input)));
+    describe("Editing children by kind and filter function", function() {
+        it("should modify selected children but no other nodes", function(){
+            var input = {
+                "no":{"A":"B"},
+                "yes":{"A":"C"},
+                "neither":{"A":"D"}
+            };
+            var expected = {
+                "no":{"A":"B"},
+                "yes":{"A":"C", "Found":"C"},
+                "neither":{"A":"D"}
+            };
 
-        expect(result).to.deep.equal(expected);
+            var result = tree.compose(
+                tree.editByKind("yes", function(n){n["Found"] = n["A"]; return n;},
+                    tree.decompose(input)));
+
+            expect(result).to.deep.equal(expected);
+        });
+    });
+
+    describe("ForEach iteration by kind and predicate", function() {
+        it("should pass only selected children to the predicate but no other nodes", function() {
+            var input = {
+                'yes': {'name': 'First', 'keep': 'yes'},
+                'no': {'name': 'Second', 'keep': 'yes'},
+                'firstChildren': {
+                    'yes': {'name': 'Third', 'keep': 'yes'},
+                    'no': {'name': 'Forth', 'keep': 'yes'}
+                },
+                'secondChildren': {
+                    'yes': {'name': 'Fifth', 'keep': 'no'},
+                    'no': {'name': 'Sixth', 'keep': 'no'}
+                },
+                'cousins': {
+                    // Test array children
+                    'yes': [
+                        {'name': 'Seventh', 'keep': 'yes'},
+                        {'name': 'Eighth', 'keep': 'no'},
+                        {'name': 'Ninth', 'keep': 'yes'}
+                    ]
+                }
+            };
+            var expectedResult = ['First', 'Third', 'Seventh', 'Ninth'].sort();
+
+            var result = [];
+            var predicate = function (n, id) {
+                if (n.keep === 'yes') {
+                    result.push(n.name);
+                }
+            };
+
+            var relational = tree.decompose(input);
+            tree.forEachByKind('yes', predicate, relational);
+            result.sort();
+
+            expect(result).to.deep.equal(expectedResult);
+        });
     });
 });
