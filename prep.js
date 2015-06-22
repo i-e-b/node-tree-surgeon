8b9f06b2a388ba4e9f472bd1b3370c3aaf47e80f
Iain Ballard <iain.ballard@gmail.com>
2015-06-22 18:28:01 +0100

    experimental tuning: decompose

---
+++ prep.js
@@ -0,0 +1,36 @@
+var fs      = require('fs');
+var metaret = require('metaret');
+
+// Build the production source from the meta-ret *.jsm
+/*console.log('compiling');
+var src = fs.readFileSync('./tree-surgeon.jsm', {encoding:'utf-8'});
+var output = metaret.jsm2js(src);
+fs.writeFileSync('./tree-surgeon-EXP.js', output);
+*/
+
+// read source and test
+var tree = require('./tree-surgeon');
+
+var bigObject = JSON.parse(fs.readFileSync('C:/Temp/big.json'));
+
+var selector = function(x){return x._meta.ID;}
+
+/*console.log('ready');
+process.stdin.resume();
+var fs = require('fs');
+var response = fs.readSync(process.stdin.fd, 100, 0, "utf8");
+process.stdin.pause();*/
+
+console.time('decomp');
+for (var i = 0; i < 10; i++){
+    var rel = tree.decompose(bigObject, ["_meta"]);
+    tree.compose(rel);
+}
+console.timeEnd('decomp');
+
+/*console.log('ready');
+process.stdin.resume();
+var fs = require('fs');
+var response = fs.readSync(process.stdin.fd, 100, 0, "utf8");
+process.stdin.pause();*/
+
